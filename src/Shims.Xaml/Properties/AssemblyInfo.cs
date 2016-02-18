00839aa2bcceba9cf86aae24edc6a61b6f2a99ad
James Ko <jamesqko@gmail.com>
2016-02-17 21:11:05 -0500

    Remove the dependency on System.Runtime.InteropServices

--- src/Shims.Xaml/Properties/AssemblyInfo.cs
+++ src/Shims.Xaml/Properties/AssemblyInfo.cs
@@ -13,11 +13,3 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a3e9dd90-a883-42a8-b27a-4fb8e1060fbf")]
